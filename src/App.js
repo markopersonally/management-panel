b1a58ce5b70e49cc912e5e98b0ee4c7cd75d883a
mkpage1337 <mrku1337@gmail.com>
2024-06-17 14:30:20 +0200

    nav styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import NavBar from "./components/NavBar";
 import Dashboard from "./components/Dashboard";
