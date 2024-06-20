91dd5ca7c7891b0043b356b8720845010303c622
mkpage1337 <mrku1337@gmail.com>
2024-06-20 18:27:52 +0200

    styling UserPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import H1 from "../components/UI/H1.jsx";
 import Section from "../components/UI/Section.jsx";
 import ImageCard from "../components/ImageCard.jsx";
 import { RxHome } from "react-icons/rx";
-import DATA from "../data.js"
+import DATA from "../data.js";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <RxHome />
         Home
       </H1>
-      <div className="p-6 bg-gray-100 text-center flex-1">
+      <div className="ml-[50px] p-6 bg-gray-100 text-center flex-1">
         <h2 className="text-2xl font-semibold mb-4">Menagment panel</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {DATA.map((item) => (
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,17 +1,26 @@
 import H1 from "../components/UI/H1.jsx";
 import Section from "../components/UI/Section.jsx";
 import { FaRegUser } from "react-icons/fa";
-
-
+import { FaPlus } from "react-icons/fa";
 
 export default function Users() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("form send");
+  }
+
   return (
     <Section>
       <H1>
         <FaRegUser />
         Users
       </H1>
-      <p>Manage your users here.</p>
+      <div className="m-10 bg-neutral-500 shadow-2xl border-2 border-neutral-700 w-[200px] h-[200px] flex flex-col gap-4 text-xl justify-center items-center hover:scale-95 duration-300">
+        <h3>Create User</h3>
+        <button onClick={handleSubmit} className="text-3xl">
+          <FaPlus />
+        </button>
+      </div>
     </Section>
   );
 }
