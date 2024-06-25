bf69161a128ff07d1a1c59ca3db9445a40cd99f6
mkpage1337 <mrku1337@gmail.com>
2024-06-25 21:47:47 +0200

    change home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,44 @@
 import H1 from "../components/UI/H1.jsx";
 import Section from "../components/UI/Section.jsx";
-import ImageCard from "../components/ImageCard.jsx";
 import { RxHome } from "react-icons/rx";
-import DATA from "../data.js";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { FaUsersLine } from "react-icons/fa6";
+import { FaTasks } from "react-icons/fa";
 
 export default function Home() {
+  const boxInfoWrapper = "flex gap-4 text-xl";
+  const boxInfo =
+    "w-[300px] h-[150px] text-orange-900 bg-orange-400 flex justify-center ";
+  const boxInfoData = "flex items-center";
+  const icon = "h-max p-2 border-2 border-orange-900 rounded-xl";
+
   return (
     <Section>
       <H1>
         <RxHome />
         Home
       </H1>
-      <div className="ml-[50px] p-6 bg-gray-100 text-center flex-1">
-        <h2 className="text-2xl font-semibold mb-4">Menagment panel</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {DATA.map((item) => (
-            <ImageCard key={item.id} title={item.title} image={item.image} />
-          ))}
+      <div className={boxInfoWrapper}>
+        <div className={boxInfo}>
+          <h3>products</h3>
+          <span className={boxInfoData}>170</span>
+          <div className={icon}>
+            <MdOutlineProductionQuantityLimits />
+          </div>
+        </div>
+        <div className={boxInfo}>
+          <h3>total customers</h3>
+          <span className={boxInfoData}>21470</span>
+          <div className={icon}>
+            <FaUsersLine />
+          </div>
+        </div>
+        <div className={boxInfo}>
+          <h3>task progress</h3>
+          <span className={boxInfoData}>77%</span>
+          <div className={icon}>
+            <FaTasks />
+          </div>
         </div>
       </div>
     </Section>
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -3,38 +3,22 @@ import Section from "../components/UI/Section.jsx";
 import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Settings() {
+  const boxChange = "mt-4 p-4 bg-slate-300";
+
   return (
     <Section>
       <H1>
         <IoSettingsOutline />
         Settings
       </H1>
-      <div>
+      <div className={boxChange}>
         <h3>Change Data</h3>
-        <div>
-          <label htmlFor="setting1">Setting 1:</label>
-          <input type="text" id="setting1" name="setting1" />
-        </div>
-        <div>
-          <label htmlFor="setting2">Setting 2:</label>
-          <input type="text" id="setting2" name="setting2" />
-        </div>
-        <div>
-          <label htmlFor="setting3">Setting 3:</label>
-          <input type="text" id="setting3" name="setting3" />
-        </div>
-        <div>
-          <label htmlFor="setting4">Setting 4:</label>
-          <input type="text" id="setting4" name="setting4" />
-        </div>
-        <div>
-          <label htmlFor="setting5">Setting 5:</label>
-          <input type="text" id="setting5" name="setting5" />
-        </div>
-        <div>
-          <label htmlFor="setting6">Setting 6:</label>
-          <input type="text" id="setting6" name="setting6" />
-        </div>
+      </div>
+      <div className={boxChange}>
+        <h3>Change Product</h3>
+      </div>
+      <div className={boxChange}>
+        <h3>Change User</h3>
       </div>
     </Section>
   );
