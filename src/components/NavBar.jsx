c2b2edc7399fa34a5268609ff75b4e0b52d2127d
mkpage1337 <mrku1337@gmail.com>
2024-07-10 21:07:13 +0200

    styling navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,15 +9,15 @@ import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 export default function NavBar() {
   const [navVisible, setNavVisible] = useState(true);
 
-  const navClasses = "absolute h-screen w-[250px] bg-slate-500";
+  const navClasses = "p-4 absolute h-max w-[250px] bg-slate-500 rounded-br-lg";
   const ulClasses = "p-8 flex flex-col gap-5 ";
   const liClasses = "border-2";
   const linkClasses =
     "w-30 p-2 flex gap-2 items-center hover:bg-slate-600 text-slate-100 text-lg duration-500";
   const btnClasses =
-    "absolute top-1/2 -right-8 transform -translate-y-1/2 p-2 bg-cyan-600 text-cyan-200 text-2xl hover:bg-cyan-900 rounded-r-lg duration-300";
+    "absolute p-2 bg-cyan-600 text-cyan-200 text-2xl hover:bg-cyan-900 rounded-r-lg duration-300";
   const btnHiddenClasses =
-    "absolute top-1/2 -left-8 transform -translate-y-1/2 p-2 bg-cyan-600 text-cyan-200 text-2xl hover:bg-cyan-900 rounded-l-lg duration-300";
+    "absolute p-2 bg-cyan-600 text-cyan-200 text-2xl hover:bg-cyan-900 rounded-l-lg duration-300";
 
   const handleShowNav = () => {
     setNavVisible(true);
@@ -78,7 +78,7 @@ export default function NavBar() {
           </li>
         </ul>
       </nav>
-      <div className="absolute top-2">
+      <div className="absolute top-2 l-2">
         {navVisible ? (
           <button className={btnClasses} onClick={handleHideNav}>
             <AiFillCaretLeft />
